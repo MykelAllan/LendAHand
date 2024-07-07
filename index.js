ba24d65df6af49dfd7dce137338cbd6333d1e511
MykelAllan <michaelallanlomugdang@gmail.com>
2024-07-06 20:48:14 -0400

    added login-register-page layout

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
     const homePage = document.getElementById('home-page'),
         landPage = document.getElementById('landing-page'),
-        notifPage = document.getElementById('notif-page');
+        notifPage = document.getElementById('notif-page'),
+        loginPage = document.getElementById('login-page'),
+        registerPage = document.getElementById('register-page');
 
     const navBtns = Array.from(document.getElementsByClassName('btn-page'));
 
@@ -16,16 +18,36 @@ window.addEventListener('DOMContentLoaded', () => {
                 homePage.style.display = 'flex';
                 landPage.style.display = 'none';
                 notifPage.style.display = 'none';
+                loginPage.style.display = 'none';
+                registerPage.style.display = 'none';
                 break;
             case "Land Page" :
                 homePage.style.display = 'none';
                 landPage.style.display = 'flex';
                 notifPage.style.display = 'none';
+                loginPage.style.display = 'none';
+                registerPage.style.display = 'none';
                 break;
             case "Notification Page" :
                 homePage.style.display = 'none';
                 landPage.style.display = 'none';
                 notifPage.style.display = 'flex';
+                loginPage.style.display = 'none';
+                registerPage.style.display = 'none';
+                break;
+            case "Login Page" :
+                homePage.style.display = 'none';
+                landPage.style.display = 'none';
+                notifPage.style.display = 'none';
+                loginPage.style.display = 'flex';
+                registerPage.style.display = 'none';
+                break;
+            case "Register Page" :
+                homePage.style.display = 'none';
+                landPage.style.display = 'none';
+                notifPage.style.display = 'none';
+                loginPage.style.display = 'none';
+                registerPage.style.display = 'flex';
                 break;
 
         }
